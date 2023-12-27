59a59152740bfe5fb3d805b425d7d2086e942518
“shamnad” <hamzashamnad123@gmail.com>
2023-12-27 11:10:46 +0530

    app.jx and css

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Monday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>Rect tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
