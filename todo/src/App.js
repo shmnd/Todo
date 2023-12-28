39ebfc49af1296afa67a7cdae9f79483f2f86000
“shamnad” <hamzashamnad123@gmail.com>
2023-12-28 16:30:04 +0530

    Todo completed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[toDos,settoDos]=useState([])
+  const[toDo,settoDo]=useState('')
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,19 +15,41 @@ function App() {
         <h2>Whoop, it's Monday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>settoDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>settoDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
       </div>
+
       <div className="todos">
-        <div className="todo">
+        {toDos.map((obj)=>{
+          return(
+            <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+              settoDos(toDos.filter(obj2=>{
+                if (obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
-        </div>
+        </div>)
+        })}
+
+        {toDos.map((obj)=>{
+          if(obj.status){
+            console.log(toDos)
+            return (<h3>{obj.text}</h3>)
+          }
+          return null
+        })}
       </div>
     </div>
   );
